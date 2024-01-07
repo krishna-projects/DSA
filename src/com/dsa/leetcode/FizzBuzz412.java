c49c8e49dc569b586d6c17d764e5b1d37c04b506
krishnandan <developer.krishnandan@gmail.com>
2024-01-07 11:57:21 +0530

    FizzBuzz412

--- src/com/dsa/leetcode/FizzBuzz412.java
+++ src/com/dsa/leetcode/FizzBuzz412.java
@@ -9,6 +9,9 @@ public class FizzBuzz412 {
         System.out.println(fizzBuzz2(100));
     }
 
+//    we can clearly see that here we are using new arraylist, so space complexity S= O(n)
+//    also we are iterating till n, so time complexity become T= O(n)
+
     public static List<String> fizzBuzz(int n) {
         List<String> result = new ArrayList<>(n);
         for (int i = 1; i <= n; i++) {
